4afa4b6933927dafd49428e70b67f36cb668db58
lamaquina27 <jdsanchezv27@gmail.com>
2025-04-09 10:10:56 -0500

    punto2 better

--- punto2.py
+++ punto2.py
@@ -5,19 +5,18 @@ import random
 def dispositivo(nombre, semaforo):
     while True:
         print(f"[{nombre}] Esperando acceso al bus...")
-        with semaforo:  # Adquiere el semáforo automáticamente y lo libera al salir del bloque
+        with semaforo:  
             print(f"[{nombre}] Accediendo al bus.")
             tiempo_uso = random.uniform(1, 3)
             time.sleep(tiempo_uso)
             print(f"[{nombre}] Liberando el bus después de {tiempo_uso:.2f} segundos.")
 
-        # Espera un poco antes de intentar usar el bus otra vez
+        
         time.sleep(random.uniform(1, 2))
 
 if __name__ == '__main__':
-    multiprocessing.set_start_method('fork')  # En Windows podrías usar 'spawn'
+    multiprocessing.set_start_method('fork')  
     
-    # Creamos un semáforo para permitir solo 1 acceso al bus a la vez
     semaforo_bus = multiprocessing.Semaphore(1)
 
     procesos = []
